3aa28978fe93dc99b2914909cf52fd6b038c5928
Viktor Holik <1viktorholik@gmail.com>
2022-11-23 15:11:07 +0100

    added responsive

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,7 @@
 const valuesItems = document.querySelectorAll(".values-item");
+const burgerMenu = document.querySelector(".burger-menu");
+const openBurgerButton = document.querySelector(".burger");
+const closeBurgerButton = document.querySelector(".cancel-icon");
 
 [...valuesItems].forEach((item, index) => {
   item.addEventListener("click", () => {
@@ -13,4 +16,13 @@ const valuesItems = document.querySelectorAll(".values-item");
     item.children[1].classList.toggle("hide");
     item.children[0].children[2].children[0].classList.toggle("rotate-plus");
   });
+});
+
+// Burger
+openBurgerButton.addEventListener("click", () => {
+  burgerMenu.classList.remove("hide");
+});
+
+closeBurgerButton.addEventListener("click", () => {
+  burgerMenu.classList.add("hide");
 });
